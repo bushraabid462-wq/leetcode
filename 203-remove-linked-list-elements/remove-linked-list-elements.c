69fbc6c12351547d4f1583f5ba6bc534ab1eb0d6
bushraabid462-wq <bushraabid462@gmail.com>
2025-11-19 21:52:37 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (58.79%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.c
@@ -0,0 +1,10 @@
+struct ListNode* removeElements(struct ListNode* head, int val){
+    struct ListNode dummy;
+    dummy.next=head;
+    struct ListNode* curr=&dummy;
+    while(curr->next){
+        if(curr->next->val==val) curr->next=curr->next->next;
+        else curr=curr->next;
+    }
+    return dummy.next;
+}
